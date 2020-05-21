059c51f44ad3ea535cc724d66f8baa48ac6778a6
Antonio Muñoz <antoniogmc@gmail.com>
2020-05-21 18:55:44 +0200

    updated graal version 20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,12 @@ repositories {
 
 dependencies {
     api("com.github.tonivade:zeromock-server:0.12.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 nativeImage {
-    setGraalVmHome("/home/slimbook/.sdkman/candidates/java/19.3.1.r11-grl")
+    setGraalVmHome("/home/slimbook/.sdkman/candidates/java/20.1.0.r8-grl")
     setMainClass("demo.HelloWorld")
     setExecutableName("hello-world")
 }
