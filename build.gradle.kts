f965c437595b4f988a7771e84c7af31720a599fc
Antonio Muñoz <antoniogmc@gmail.com>
2020-05-22 23:53:30 +0200

    added fatJar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.jvm.tasks.Jar
+
 plugins {
     `java-library`
     id("org.mikeneck.graalvm-native-image") version "0.5.0"
@@ -15,4 +17,20 @@ nativeImage {
     setGraalVmHome("/home/slimbook/.sdkman/candidates/java/20.1.0.r8-grl")
     setMainClass("demo.HelloWorld")
     setExecutableName("hello-world")
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "demo.HelloWorld"
+    }
+    classifier = "all"
+    from(configurations.compileClasspath.get().map({ if (it.isDirectory()) it else zipTree(it) }))
+    from(configurations.runtimeClasspath.get().map({ if (it.isDirectory()) it else zipTree(it) }))
+    with(tasks["jar"] as CopySpec)
 }
