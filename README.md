9eaee2114df3ecb560dc8d10234e6ee5b8df68fb
Antonio Muñoz <antoniogmc@gmail.com>
2020-05-22 20:40:38 +0200

    added license and updated readme

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 This is a example application to test native image
 
+Install sdkman, and then install graalvm:
+
+```shell
+$ curl -s "https://get.sdkman.io" | bash
+$ sdk install java 20.1.0.r8-grl
+```
+
+And then, build the native image:
+
 ```shell
 $ ./gradlew clean nativeImage
 $ ./build/native-image/hello-world
 ```
+
+Java 8 is required also.
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,16 +9,10 @@ repositories {
 
 dependencies {
     api("com.github.tonivade:zeromock-server:0.12.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 nativeImage {
     setGraalVmHome("/home/slimbook/.sdkman/candidates/java/20.1.0.r8-grl")
     setMainClass("demo.HelloWorld")
     setExecutableName("hello-world")
-}
-
-val test by tasks.getting(Test::class) {
-    useJUnitPlatform()
 }
